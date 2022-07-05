7139b613c02dbd8853116e66d34c6ffa52fc00ee
birdmoney11 <34954336+birdmoney11@users.noreply.github.com>
2022-07-05 22:39:46 +0800

    a
    aa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mysql = require("mysql");
-const port = 8089;
+const port = 8088;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   "/css",
@@ -14,18 +14,21 @@ app.use(express.static('views'))
 app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
 app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "322300",
-  database: "myDevices"
+  host: "137.184.191.242",
+  user: "sammy2",
+  password: "Asahi186!",
+  database: "epettracker"
 });
 // connect to database
+
 db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log("Connected to database");
 });
+
+
 global.db = db;
 require("./routes/main")(app);
 app.set("views", __dirname + "/views");
