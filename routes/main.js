f6b8762394d32ed3eec99c0b7efa139b5ee50a71
birdmoney11 <34954336+birdmoney11@users.noreply.github.com>
2022-08-28 16:06:59 +0800

    fixed deletepet function
    fixed deletepet function, changed to walk_3; feed_3

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ const express = require("express");
 module.exports = function (app) {
   app.get("/", function (req, res) {
     let sqlquery =
-      "SELECT * FROM walk_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= NOW() ORDER BY walk_datetime ASC LIMIT 1; SELECT * FROM feed_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE feed_datetime >= NOW() ORDER BY feed_datetime ASC LIMIT 1;";
+      "SELECT * FROM walk_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= NOW() ORDER BY walk_datetime ASC LIMIT 1; SELECT * FROM feed_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE feed_datetime >= NOW() ORDER BY feed_datetime ASC LIMIT 1;";
     // execute sql query
     db.query(sqlquery, (err, result) => {
       if (err) {
@@ -23,7 +23,7 @@ module.exports = function (app) {
 // Andy Code==========================================================================
   // render pet page in database
   app.get("/pet", function (req, res) {
-    let sqlquery = "SELECT * FROM pet_test1; SELECT * FROM walk_1; SELECT * FROM feed_1";
+    let sqlquery = "SELECT * FROM pet_test1; SELECT * FROM walk_3; SELECT * FROM feed_3";
     // execute sql query
     db.query(sqlquery, (err, result) => {
       if (err) {
@@ -59,7 +59,7 @@ module.exports = function (app) {
     };
     if (req.body.add_walk) {
       let sql =
-        "INSERT INTO walk_1 (walk_name, walk_distance, walk_datetime, pet_id) VALUES (?, ?, ?, ?)";
+        "INSERT INTO walk_3 (walk_name, walk_distance, walk_datetime, pet_id) VALUES (?, ?, ?, ?)";
       for (const key in req.body) {
         let new_records = req.body[key];
         // execute sql query
@@ -75,7 +75,7 @@ module.exports = function (app) {
     if (req.body.add_feed) {
       console.log(req.body);
       let sql =
-        "INSERT INTO feed_1 (feed_name, feed_type, feed_amount, feed_datetime, pet_id) VALUES (?, ?, ?, ?, ?)";
+        "INSERT INTO feed_3 (feed_name, feed_type, feed_amount, feed_datetime, pet_id) VALUES (?, ?, ?, ?, ?)";
       for (const key in req.body) {
         let new_records = req.body[key];
         // execute sql query
@@ -94,7 +94,7 @@ module.exports = function (app) {
   app.get("/delete_walk/:id", (req, res) => {
     // res.send('this is delete walk');
     const walkId = req.params.id;
-    let sql = `DELETE from walk_1 where walk_id = ${walkId}`;
+    let sql = `DELETE from walk_3 where walk_id = ${walkId}`;
     let query = db.query(sql, (err, result) => {
       if (err) throw err;
       res.redirect('/pet');
@@ -104,7 +104,7 @@ module.exports = function (app) {
   // delete feed by id
   app.get("/delete_feed/:id", (req, res) => {
     const feedId = req.params.id;
-    let sql = `DELETE from feed_1 where feed_id = ${feedId}`;
+    let sql = `DELETE from feed_3 where feed_id = ${feedId}`;
     let query = db.query(sql, (err, result) => {
       if (err) throw err;
       res.redirect('/pet');
@@ -143,7 +143,7 @@ module.exports = function (app) {
   app.get("/todo", function (req, res) {
      // get mysql records for [0]today; [1]today +1 and +2; [2]today +2 and today +93 according to xxxx_timestamp; max up to + 365 days 
     let sqlquery =
-      "SELECT * from walk_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() AND a.walk_datetime < CURDATE() + INTERVAL 1 DAY ORDER BY a.walk_datetime; SELECT * from walk_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() + INTERVAL 1 DAY AND a.walk_datetime < CURDATE() + INTERVAL 2 DAY ORDER BY a.walk_datetime; SELECT * from walk_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() + INTERVAL 2 DAY AND a.walk_datetime < CURDATE() + INTERVAL 365 DAY ORDER BY a.walk_datetime; SELECT * from feed_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() AND a.feed_datetime < CURDATE() + INTERVAL 1 DAY ORDER BY a.feed_datetime; SELECT * from feed_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() + INTERVAL 1 DAY AND a.feed_datetime < CURDATE() + INTERVAL 2 DAY ORDER BY a.feed_datetime; SELECT * from feed_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() + INTERVAL 2 DAY AND a.feed_datetime < CURDATE() + INTERVAL 365 DAY ORDER BY a.feed_datetime";
+      "SELECT * from walk_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() AND a.walk_datetime < CURDATE() + INTERVAL 1 DAY ORDER BY a.walk_datetime; SELECT * from walk_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() + INTERVAL 1 DAY AND a.walk_datetime < CURDATE() + INTERVAL 2 DAY ORDER BY a.walk_datetime; SELECT * from walk_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.walk_datetime >= CURDATE() + INTERVAL 2 DAY AND a.walk_datetime < CURDATE() + INTERVAL 365 DAY ORDER BY a.walk_datetime; SELECT * from feed_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() AND a.feed_datetime < CURDATE() + INTERVAL 1 DAY ORDER BY a.feed_datetime; SELECT * from feed_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() + INTERVAL 1 DAY AND a.feed_datetime < CURDATE() + INTERVAL 2 DAY ORDER BY a.feed_datetime; SELECT * from feed_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.feed_datetime >= CURDATE() + INTERVAL 2 DAY AND a.feed_datetime < CURDATE() + INTERVAL 365 DAY ORDER BY a.feed_datetime";
     // execute sql query
     db.query(sqlquery, (err, result) => {
       if (err) {
@@ -164,7 +164,7 @@ module.exports = function (app) {
   // post for updating walk_complete boolean in todo.html
   app.post("/update_walk_complete", function (req, res) {
     // saving data in database
-    let sqlquery = "UPDATE walk_1 SET walk_complete = (?) WHERE walk_id = (?);";
+    let sqlquery = "UPDATE walk_3 SET walk_complete = (?) WHERE walk_id = (?);";
     // execute sql query
     let updaterecord = [req.body.walk_complete, req.body.walk_id];
     db.query(sqlquery, updaterecord, (err, result) => {
@@ -177,7 +177,7 @@ module.exports = function (app) {
   // post for updating feed_complete boolean in todo.html
   app.post("/update_feed_complete", function (req, res) {
     // saving data in database
-    let sqlquery = "UPDATE feed_1 SET feed_complete = (?) WHERE feed_id = (?);";
+    let sqlquery = "UPDATE feed_3 SET feed_complete = (?) WHERE feed_id = (?);";
     // execute sql query
     let updaterecord = [req.body.feed_complete, req.body.feed_id];
     db.query(sqlquery, updaterecord, (err, result) => {
@@ -191,7 +191,7 @@ module.exports = function (app) {
   // post for updating feed_complete boolean in todo.html
   app.post("/update_feed_complete", function (req, res) {
     // saving data in database
-    let sqlquery = "UPDATE feed_1 SET feed_complete = (?) WHERE feed_id = (?);";
+    let sqlquery = "UPDATE feed_3 SET feed_complete = (?) WHERE feed_id = (?);";
     // execute sql query
     let updaterecord = [req.body.feed_complete, req.body.feed_id];
     db.query(sqlquery, updaterecord, (err, result) => {
@@ -206,7 +206,7 @@ module.exports = function (app) {
   app.get("/new_status", function (req, res) {
     // query database to get all the pets
     let sqlquery =
-      "SELECT * FROM pet_test1; SELECT * FROM walk_1; SELECT * FROM feed_1; SELECT * FROM walk_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 1; SELECT * FROM walk_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 2; SELECT * FROM walk_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 3; SELECT * FROM feed_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 1; SELECT * FROM feed_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 2; SELECT * FROM feed_1 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 3";
+      "SELECT * FROM pet_test1; SELECT * FROM walk_3; SELECT * FROM feed_3; SELECT * FROM walk_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 1; SELECT * FROM walk_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 2; SELECT * FROM walk_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 3; SELECT * FROM feed_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 1; SELECT * FROM feed_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 2; SELECT * FROM feed_3 a left JOIN pet_test1 b ON a.pet_id = b.pet_id WHERE a.pet_id = 3";
     // execute sql query
     db.query(sqlquery, (err, result) => {
       if (err) {
@@ -229,7 +229,7 @@ module.exports = function (app) {
   // post for removewalk button in new_status page
   app.post("/removewalk", function (req, res) {
     // saving data in database
-    let sqlquery = "DELETE from walk_1 where walk_id = (?)";
+    let sqlquery = "DELETE from walk_3 where walk_id = (?)";
     // execute sql query
     let updaterecord = [req.body.id];
     db.query(sqlquery, updaterecord, (err, result) => {
@@ -277,7 +277,7 @@ module.exports = function (app) {
     if (req.body.add_walk) {
       // console.log(req.body);
       let sqlquery2 =
-        "INSERT INTO walk_1 (walk_name, walk_distance, walk_datetime, pet_id) VALUES (?, ?, ?, ?)";
+        "INSERT INTO walk_3 (walk_name, walk_distance, walk_datetime, pet_id) VALUES (?, ?, ?, ?)";
 
       for (const key in req.body) {
         let new_records = req.body[key];
@@ -299,7 +299,7 @@ module.exports = function (app) {
     if (req.body.add_feed) {
       // console.log(req.body);
       let sqlquery2 =
-        "INSERT INTO feed_1 (feed_name, feed_type, feed_amount, feed_datetime, pet_id) VALUES (?, ?, ?, ?, ?)";
+        "INSERT INTO feed_3 (feed_name, feed_type, feed_amount, feed_datetime, pet_id) VALUES (?, ?, ?, ?, ?)";
 
       for (const key in req.body) {
         let new_records = req.body[key];
@@ -438,7 +438,7 @@ module.exports = function (app) {
    // render delete page 
     app.get("/delete", function (req, res) {
         // query database to get all the entries 
-        let sqlquery = "SELECT * FROM pet_test1; SELECT * from walk_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id ORDER BY walk_datetime; SELECT * from feed_1 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id ORDER BY feed_datetime";
+        let sqlquery = "SELECT * FROM pet_test1; SELECT * from walk_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id ORDER BY walk_datetime; SELECT * from feed_3 a LEFT JOIN pet_test1 b ON a.pet_id = b.pet_id ORDER BY feed_datetime";
         // execute sql query
         db.query(sqlquery, (err, result) => {
             if (err) {
@@ -455,24 +455,24 @@ module.exports = function (app) {
 // handle form data from deleteForm submission
     app.post("/deletePet", function (req, res) {
         // query database to get all the devices
-        let sqlquery = "SET foreign_key_checks = 0; DELETE FROM `pet_test1` WHERE pet_id = (?); SET foreign_key_checks = 1;"
+        let sqlquery = "DELETE FROM `pet_test1` WHERE pet_id = (?)"
         // execute sql query
         for (const key in req.body) {
                 let new_records = req.body[key];
-                res.write(" This entry has been deleted from the database, name: " + req.body[key]);
+                //res.write(" This entry has been deleted from the database, name: " + req.body[key]);
                 // execute sql query
                 db.query(sqlquery, new_records, (err, result) => {
                     if (err) {
                         return console.error(err.message);
                     } else {
-                        res.send()
+                      res.redirect("/delete")
                     }
                 });
             }
         });
     app.post("/deleteWalk", function (req, res) {
         // query database to get all the devices
-        let sqlquery = "DELETE FROM `walk_1` WHERE walk_id = (?);"
+        let sqlquery = "DELETE FROM `walk_3` WHERE walk_id = (?);"
         // execute sql query
         for (const key in req.body) {
                 let new_records = req.body[key];
@@ -489,7 +489,7 @@ module.exports = function (app) {
         });
     app.post("/deleteFeeding", function (req, res) {
         // query database to get all the devices
-        let sqlquery = "DELETE FROM `feed_1` WHERE feed_id = (?);"
+        let sqlquery = "DELETE FROM `feed_3` WHERE feed_id = (?);"
         // execute sql query
         for (const key in req.body) {
                 let new_records = req.body[key];
